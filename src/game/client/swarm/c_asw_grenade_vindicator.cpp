eaa398d4f5f404205c372c5f46177f0f00876e47
jhh8 <jhh8bitbucket@gmail.com>
2025-02-23 20:37:23 +0200

    fix vindinade particles and sound disappearing with packet loss

---
+++ src/game/client/swarm/c_asw_grenade_vindicator.cpp
@@ -0,0 +1,57 @@
+#include "cbase.h"
+#include "c_asw_grenade_vindicator.h"
+
+// memdbgon must be the last include file in a .cpp file!!!
+#include "tier0/memdbgon.h"
+
+IMPLEMENT_NETWORKCLASS_ALIASED( ASW_Grenade_Vindicator, DT_ASW_Grenade_Vindicator )
+
+BEGIN_NETWORK_TABLE( C_ASW_Grenade_Vindicator, DT_ASW_Grenade_Vindicator )
+	RecvPropVector( RECVINFO( m_vecDetonateOrigin ) ),
+END_NETWORK_TABLE()
+
+
+C_ASW_Grenade_Vindicator::C_ASW_Grenade_Vindicator()
+{
+	m_bDetonated = false;
+	m_pSmokeTrail = NULL;
+	m_vecDetonateOrigin = Vector( 0.0, 0.0, 0.0 );
+
+	EmitSound( "ASWGrenade.Alarm" );
+}
+
+void C_ASW_Grenade_Vindicator::OnDataChanged(DataUpdateType_t updateType)
+{
+	if ( updateType == DATA_UPDATE_CREATED )
+		CreateSmokeTrail();
+
+	if ( updateType == DATA_UPDATE_DATATABLE_CHANGED )
+	{
+		if ( !m_bDetonated && m_vecDetonateOrigin != Vector( 0.0, 0.0, 0.0 ) )
+		{
+			Detonate();
+			m_bDetonated = true;
+		}
+	}
+}
+
+void C_ASW_Grenade_Vindicator::CreateSmokeTrail()
+{
+	if ( m_pSmokeTrail )
+		return;
+
+	m_pSmokeTrail = ParticleProp()->Create( "rocket_trail_small", PATTACH_ABSORIGIN_FOLLOW, -1, Vector( 0, 0, 0 ) );
+}
+
+void C_ASW_Grenade_Vindicator::Detonate()
+{
+	if ( m_pSmokeTrail )
+	{
+		m_pSmokeTrail->StopEmission();
+		m_pSmokeTrail = NULL;
+	}
+
+	StopSound( "ASWGrenade.Alarm" );
+	EmitSound( "ASWGrenade.Incendiary" );
+	DispatchParticleEffect( "vindicator_grenade", m_vecDetonateOrigin, GetLocalAngles() );
+}
