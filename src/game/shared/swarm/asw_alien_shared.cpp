99afb8ff8636346c9031f3c1a8d36be9abfbc38d
kitsune <sekwensernl@gmail.com>
2025-11-08 13:25:01 +0100

    fix vertical alien blood color

--- src/game/shared/swarm/asw_alien_shared.cpp
+++ src/game/shared/swarm/asw_alien_shared.cpp
@@ -110,7 +110,7 @@ void CASW_Alien::DoBloodDecal( float flDamage, const Vector &vecPos, const Vecto
 		return;
 
 	// make blood decal on the wall!
-	trace_t Bloodtr;
+	trace_t Bloodtr, Bloodtr2;
 	Vector vecTraceDir;
 	float flNoise;
 	int cCount;
@@ -159,10 +159,15 @@ void CASW_Alien::DoBloodDecal( float flDamage, const Vector &vecPos, const Vecto
 
 		// Don't bleed on grates.
 		UTIL_TraceLine( vecPos, vecPos + vecTraceDir * -flTraceDist, MASK_SHOT_HULL & ~CONTENTS_GRATE, this, COLLISION_GROUP_NONE, &Bloodtr);
+		UTIL_TraceLine( vecPos, vecPos + vecTraceDir * -flTraceDist, MASK_SHOT_HULL, this, COLLISION_GROUP_NONE, &Bloodtr2 );
 
 		if ( Bloodtr.fraction != 1.0 )
 		{
 			UTIL_BloodDecalTrace( &Bloodtr, BloodColor() );
+		}
+		if ( Bloodtr2.fraction != 1.0 && Bloodtr.endpos != Bloodtr2.endpos )
+		{
+			UTIL_BloodDecalTrace( &Bloodtr2, BloodColor() );
 		}
 	}
 }
