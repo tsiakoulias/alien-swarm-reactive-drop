382313af70610cc623a769ffa4aff66b12fb87f2
Ioannis Marios Tsiakoulias <tsiakoulias@ys.edu.gr>
2026-02-25 23:21:22 +0200

    Fix dedicated restart crash: gate Steam stats until activated

--- src/game/server/swarm/asw_player.h
+++ src/game/server/swarm/asw_player.h
@@ -278,6 +278,8 @@ public:
 	bool m_bHasAwardedXP;
 	bool m_bPendingSteamStats;
 	float m_flPendingSteamStatsStart;
+	bool m_bDeferredSteamStatsRequest;
+	float m_flDeferredSteamStatsRequestStart;
 	bool m_bSentPromotedMessage;
 
 	// static inventory (medals)
